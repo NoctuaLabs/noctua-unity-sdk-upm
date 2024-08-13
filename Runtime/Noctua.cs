db095b372c61943e370c06f30a7193f3bdd2cea6
Herpiko Dwi Aguno <herpiko@noctua.gg>
2024-08-13 21:38:28 +0700

    Fix config load by using UnityWebRequest.

--- Runtime/Noctua.cs
+++ Runtime/Noctua.cs
@@ -2,19 +2,44 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
 
 namespace com.noctuagames.sdk
 {
-    public class NoctuaConfig
-    {
-        [JsonProperty("baseUrl")] public string BaseUrl;
-    }
 
-    public class GlobalConfig
-    {
-        [JsonProperty("noctua")] public NoctuaConfig Noctua;
-        [JsonProperty("clientId")] public string ClientId;
-    }
+    public class AdjustConfig
+{
+    [JsonProperty("appToken")]
+    public string AppToken { get; set; }
+
+    [JsonProperty("environment")]
+    public string Environment { get; set; }
+
+    [JsonProperty("eventMap")]
+    public Dictionary<string, string> EventMap { get; set; }
+}
+
+public class NoctuaConfig
+{
+    [JsonProperty("trackerUrl")]
+    public string TrackerUrl { get; set; }
+
+    [JsonProperty("baseUrl")]
+    public string BaseUrl { get; set; }
+}
+
+public class GlobalConfig
+{
+    [JsonProperty("clientId")]
+    public string ClientId { get; set; }
+
+    [JsonProperty("adjust")]
+    public AdjustConfig Adjust { get; set; }
+
+    [JsonProperty("noctua")]
+    public NoctuaConfig Noctua { get; set; }
+}
 
     public class Noctua
     {
@@ -23,8 +48,26 @@ namespace com.noctuagames.sdk
 
         static Noctua()
         {
-            var configPath = Application.streamingAssetsPath + "/noctuagg.json";
-            var config = JsonConvert.DeserializeObject<GlobalConfig>(System.IO.File.ReadAllText(configPath));
+            GlobalConfig config = new GlobalConfig();
+            Debug.Log("Loading streaming assets...");
+            var configPath = Path.Combine(Application.streamingAssetsPath, "noctuagg.json");
+            Debug.Log(configPath);
+            var loadingRequest = UnityWebRequest.Get(configPath);
+            loadingRequest.SendWebRequest();
+            while (!loadingRequest.isDone) {
+                if (loadingRequest.result == UnityWebRequest.Result.ProtocolError) {
+                    Debug.Log("Loading streaming assets: loadingRequest ProtocolError");
+                    break;
+                }
+            }
+            if (loadingRequest.result == UnityWebRequest.Result.ProtocolError) {
+                    Debug.Log("Loading streaming assets: loadingRequest ProtocolError");
+            } else {
+                File.WriteAllBytes(Path.Combine(Application.persistentDataPath , "your.bytes"), loadingRequest.downloadHandler.data);
+                string jsonStr;
+                jsonStr = System.Text.Encoding.UTF8.GetString(loadingRequest.downloadHandler.data, 3, loadingRequest.downloadHandler.data.Length - 3);
+                config = JsonConvert.DeserializeObject<GlobalConfig>(jsonStr);
+            }
 
             Auth = new NoctuaAuthService(
                 new NoctuaAuthService.Config
@@ -33,6 +76,7 @@ namespace com.noctuagames.sdk
                     ClientId = config.ClientId
                 }
             );
+
         }
 
         public static void Init()
