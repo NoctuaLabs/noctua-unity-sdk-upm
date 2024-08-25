0127f1573aa95d5d2a5731ff99563d7d9585827a
Wijaya Adhi Surya <wijaya.adhisurya@gmail.com>
2024-08-26 01:54:49 +0700

    feat: add player avatar

---
+++ Runtime/UI/LoginOptionsDialogPresenter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace com.noctuagames.sdk.UI
+{
+    public class LoginOptionsDialogPresenter : Presenter<object>
+    {
+        protected override void Attach()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Detach()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
