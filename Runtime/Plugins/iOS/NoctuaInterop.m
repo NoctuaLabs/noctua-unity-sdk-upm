65af750cb2be4b2cc6dcaceab62e970c31ab525e
Wijaya Adhi Surya <wijaya.adhisurya@gmail.com>
2024-09-10 06:55:10 +0700

    fix: update iOS SDK version to fix JSON serialization crash

--- Runtime/Plugins/iOS/NoctuaInterop.m
+++ Runtime/Plugins/iOS/NoctuaInterop.m
@@ -9,6 +9,8 @@ void noctuaInitialize(void) {
 }
 
 void noctuaTrackAdRevenue(const char* source, double revenue, const char* currency, const char* extraPayloadJson) {
+    NSLog(@"source: %s, revenue: %f, currency: %s, extraPayload: %s", source, revenue, currency, extraPayloadJson);
+
     NSString *sourceStr = [NSString stringWithUTF8String:source];
     NSString *currencyStr = [NSString stringWithUTF8String:currency];
     NSString *extraPayloadStr = [NSString stringWithUTF8String:extraPayloadJson];
@@ -17,37 +19,25 @@ void noctuaTrackAdRevenue(const char* source, double revenue, const char* curren
     [Noctua trackAdRevenueWithSource:sourceStr revenue:revenue currency:currencyStr extraPayload:extraPayload];
     // log params
 
-    NSLog(@"source: %s", source);
-    NSLog(@"revenue: %f", revenue);
-    NSLog(@"currency: %s", currency);
-    NSLog(@"extraPayload: %s", extraPayloadJson);
 }
 
 void noctuaTrackPurchase(const char* orderId, double amount, const char* currency, const char* extraPayloadJson) {
+    NSLog(@"orderId: %s, amount: %f, currency: %s, extraPayload: %s", orderId, amount, currency, extraPayloadJson);
+
     NSString *orderIdStr = [NSString stringWithUTF8String:orderId];
     NSString *currencyStr = [NSString stringWithUTF8String:currency];
     NSString *extraPayloadStr = [NSString stringWithUTF8String:extraPayloadJson];
     NSData *data = [extraPayloadStr dataUsingEncoding:NSUTF8StringEncoding];
     NSDictionary *extraPayload = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
     [Noctua trackPurchaseWithOrderId:orderIdStr amount:amount currency:currencyStr extraPayload:extraPayload];
-
-    // log params
-
-    NSLog(@"orderId: %s", orderId);
-    NSLog(@"amount: %f", amount);
-    NSLog(@"currency: %s", currency);
-    NSLog(@"extraPayload: %s", extraPayloadJson);
 }
 
 void noctuaTrackCustomEvent(const char* eventName, const char* payloadJson) {
+    NSLog(@"eventName: %s, payload: %s", eventName, payloadJson);
+
     NSString *eventNameStr = [NSString stringWithUTF8String:eventName];
     NSString *payloadStr = [NSString stringWithUTF8String:payloadJson];
     NSData *data = [payloadStr dataUsingEncoding:NSUTF8StringEncoding];
     NSDictionary *payload = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
     [Noctua trackCustomEvent:eventNameStr payload:payload];
-
-    // log params
-
-    NSLog(@"eventName: %s", eventName);
-    NSLog(@"payload: %s", payloadJson);
 }
