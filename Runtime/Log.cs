5a6e928ed33d5a63ba7b1c1fe76b34a57cdc6efd
Wijaya Adhi Surya <wijaya.adhisurya@gmail.com>
2024-10-10 19:38:06 +0700

    chore: split long log

--- Runtime/Log.cs
+++ Runtime/Log.cs
@@ -1,6 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace com.noctuagames.sdk
 {
@@ -24,17 +26,41 @@ namespace com.noctuagames.sdk
         
         public void Log(string message, [CallerMemberName] string caller = "")
         {
-            UnityEngine.Debug.unityLogger.Log(LogType.Log, $"[NoctuaSDK {_typeName}.{caller}]", message);
+            Log(LogType.Log, message, caller);
         }
         
         public void Warning(string message, [CallerMemberName] string caller = "")
         {
-            UnityEngine.Debug.unityLogger.Log(LogType.Warning, $"[NoctuaSDK {_typeName}.{caller}]", message);
+            Log(LogType.Warning, message, caller);
         }
 
         public void Error(string message, [CallerMemberName] string caller = "")
         {
-            UnityEngine.Debug.unityLogger.Log(LogType.Error, $"[NoctuaSDK {_typeName}.{caller}]", message);
+            Log(LogType.Error, message, caller);
+        }
+
+        private void Log(LogType logType, string message, string caller = "")
+        {
+            var chunkSize = 800;
+
+            if (message.Length < chunkSize)
+            {
+                Debug.unityLogger.Log(logType, $"[NoctuaSDK {_typeName}.{caller}]", message);
+                
+                return;
+            }
+
+            for (var i = 0; i < message.Length; i += chunkSize)
+            {
+                var chunk = message.Substring(i, Math.Min(chunkSize, message.Length - i));
+                var totalChunks = (message.Length / chunkSize) + 1;
+                var part = (i / chunkSize) + 1;
+                
+                var prefix = part == 1 ? "[BEGIN]" : "[CONT]";
+                var suffix = part == totalChunks ? "[END]" : "[CONT]";
+                
+                Debug.unityLogger.Log(logType, $"[NoctuaSDK {_typeName}.{caller} {part}/{totalChunks}]", prefix + chunk + suffix);
+            }
         }
     }
 }
