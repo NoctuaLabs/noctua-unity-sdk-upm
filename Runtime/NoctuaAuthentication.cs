9b95c1932ee82c68e1ce10c5fa9de146667b29bf
EVOS-LP22-0007\User <mujiburrohman@noctua.gg>
2024-11-18 21:04:17 +0700

    refactor: method authenticateAsync

--- Runtime/NoctuaAuthentication.cs
+++ Runtime/NoctuaAuthentication.cs
@@ -84,9 +84,22 @@ namespace com.noctuagames.sdk
         /// Returns the authenticated user bundle.
         /// </summary>
         /// <returns>A UserBundle object representing the selected account.</returns>
-        public async UniTask<UserBundle> AuthenticateAsync()
+        public async UniTask<(UserBundle userBundle, Exception exception)> AuthenticateAsync()
         {
-            return await _service.AuthenticateAsync();
+            try
+            {
+                var userBundle = await _service.AuthenticateAsync();
+                return (userBundle, null);
+            }
+            catch (NoctuaException noctuaEx) when (noctuaEx.ErrorCode == (int)NoctuaErrorCode.UserBanned)
+            {
+                _uiFactory.ShowGeneralConfirmDialog();
+                return (null, noctuaEx);
+            }
+            catch (Exception ex)
+            {
+                return (null, ex);
+            }
         }
 
         public async UniTask<UserBundle> LoginAsGuest()
