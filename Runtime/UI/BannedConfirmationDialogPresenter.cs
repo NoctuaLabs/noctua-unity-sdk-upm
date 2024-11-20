d2aa8c2deb2dd53978256bd80893d59c70ff3897
EVOS-LP22-0007\User <mujiburrohman@noctua.gg>
2024-11-20 15:53:10 +0700

    refactor: rename with spesific name banned confirmation dialog

---
+++ Runtime/UI/BannedConfirmationDialogPresenter.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Cysharp.Threading.Tasks;
+
+namespace com.noctuagames.sdk.UI
+{
+    internal class BannedConfirmationDialogPresenter : Presenter<object>
+    {
+        private Button _btnOK;
+        private Label _note;
+        private Label _note2;
+
+        private readonly ILogger _log = new NoctuaLogger(typeof(BannedConfirmationDialogPresenter));
+
+        private UniTaskCompletionSource<bool> _tcs;
+
+        protected override void Attach()
+        {}
+
+        protected override void Detach()
+        {}
+
+        private void Start()
+        {
+            _btnOK = View.Q<Button>("OKButton");
+            _note = View.Q<Label>("Note");
+            _note2 = View.Q<Label>("Note2");
+
+            _note2.RegisterCallback<PointerUpEvent>(OpenCS);
+            _btnOK.RegisterCallback<PointerUpEvent>(CloseDialog);
+        }
+
+        public void Show(string regionCode, UniTaskCompletionSource<bool> tcs)
+        {
+            Visible = true;
+            _tcs = tcs;
+
+            _note.text = Utility.GetTranslation("User.Banned.Info",  Utility.LoadTranslations(regionCode));
+            _note2.text = Utility.GetTranslation("User.Banned.Info2",  Utility.LoadTranslations(regionCode));
+        }
+
+        private async void OpenCS(PointerUpEvent evt)
+        {
+            try
+            {
+                Visible = false;
+
+                await Noctua.Platform.Content.ShowCustomerService();
+
+                _log.Info("Customer Service URL opened");
+
+                _tcs?.TrySetResult(true);
+
+            } 
+            catch (Exception e) {
+
+                Visible = false;
+
+                _tcs?.TrySetResult(true);
+
+                if (e is NoctuaException noctuaEx)
+                {
+                    _log.Info("NoctuaException: " + noctuaEx.ErrorCode + " : " + noctuaEx.Message);
+                } else {
+                    _log.Info("Exception: " + e);
+                }
+            }
+        }
+
+        private void CloseDialog(PointerUpEvent evt)
+        {
+            Visible = false;
+            _tcs?.TrySetResult(true);
+        }
+    }
+}
