467cf1119433014633b3e8322bbfc3e26f790d46
Herpiko Dwi Aguno <herpiko@gmail.com>
2024-12-19 14:04:58 +0700

    fix: Add null check on register by email verification.

--- Runtime/UI/EmailVerificationDialogPresenter.cs
+++ Runtime/UI/EmailVerificationDialogPresenter.cs
@@ -190,17 +190,17 @@ namespace com.noctuagames.sdk.UI
             View?.Q<VisualElement>("DialogHeader")?.AddToClassList("hide");
             try
             {
-                if (Model.AuthService.RecentAccount == null &&
-                (Model.AuthService.RecentAccount != null && Model.AuthService.RecentAccount.IsGuest))
+                if (Model.AuthService.RecentAccount == null ||
+                !(Model.AuthService.RecentAccount != null && Model.AuthService.RecentAccount.IsGuest))
                 {
-                    // There will be a confirmation dialog between verification processes.
-                    var token = await Model.AuthService.BeginVerifyEmailRegistrationAsync(_credVerifyId, _credVerifyCode);
-                    Model.ShowBindConfirmation(token);
+                    // Verify directly
+                    await Model.VerifyEmailRegistration(_credVerifyId, _credVerifyCode);
                 }
                 else
                 {
-                    // Verify directly without confirmation dialog.
-                    await Model.VerifyEmailRegistration(_credVerifyId, _credVerifyCode);
+                    // There will be a confirmation dialog between verification processes
+                    var token = await Model.AuthService.BeginVerifyEmailRegistrationAsync(_credVerifyId, _credVerifyCode);
+                    Model.ShowBindConfirmation(token);
                 }
 
                 Visible = false;
