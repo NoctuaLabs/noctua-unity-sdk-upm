0ea32ea684dc22b6ec1c258460f9008311b0c165
Mujiburrohman <mujiburr741@gmail.com>
2024-12-20 21:14:11 +0700

    fix: date picker ios

--- Runtime/Plugins/iOS/DatePicker/IOSNativeDatePicker.mm
+++ Runtime/Plugins/iOS/DatePicker/IOSNativeDatePicker.mm
@@ -2,78 +2,56 @@
 
 @implementation IOSNativeDatePicker
 
-+ (CGFloat) GetW {
-    
-    UIViewController *vc =  UnityGetGLViewController();
-    
-    bool IsLandscape = true;
-    
-    
++ (CGFloat)GetW {
+    UIViewController *vc = UnityGetGLViewController();
     UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
-    if(orientation == UIInterfaceOrientationLandscapeLeft || orientation == UIInterfaceOrientationLandscapeRight) {
-        IsLandscape = true;
-    } else {
-        IsLandscape = false;
+    BOOL isLandscape = UIInterfaceOrientationIsLandscape(orientation);
+    CGFloat width = isLandscape ? vc.view.frame.size.height : vc.view.frame.size.width;
+
+    // Adjust for iOS 8+ where the frame size does not rotate with orientation
+    if (@available(iOS 8.0, *)) {
+        width = vc.view.frame.size.width;
     }
-    
-    CGFloat w;
-    if(IsLandscape) {
-        w = vc.view.frame.size.height;
-    } else {
-        w = vc.view.frame.size.width;
-    }
-    
-    
-    NSArray *vComp = [[UIDevice currentDevice].systemVersion componentsSeparatedByString:@"."];
-    if ([[vComp objectAtIndex:0] intValue] >= 8) {
-        w = vc.view.frame.size.width;
-    }
-    
-    
-    return w;
+
+    return width;
 }
 
-+ (void) DP_changeDate:(UIDatePicker *)sender {
++ (void)DP_changeDate:(UIDatePicker *)sender {
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
     NSString *dateString = [dateFormatter stringFromDate:sender.date];
-
-    // Get the pickerId from the datePicker's tag
     int pickerId = (int)sender.tag;
 
-    // Include the pickerId in the Unity message
     NSString *unityObjectName = [NSString stringWithFormat:@"MobileDateTimePicker_%d", pickerId];
-    
     UnitySendMessage([unityObjectName UTF8String], "DateChangedEvent", [dateString UTF8String]);
 }
 
-+ (void) DP_PickerClosed:(UIDatePicker *)sender {
++ (void)DP_doneButtonClicked:(UIBarButtonItem *)sender {
+    [self DP_dismissDatePicker:nil];
+}
+
++ (void)DP_PickerClosed:(UIDatePicker *)sender {
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
     NSString *dateString = [dateFormatter stringFromDate:sender.date];
-    
-    // Get the pickerId from the datePicker's tag
     int pickerId = (int)sender.tag;
 
-    // Include the pickerId in the Unity message
     NSString *unityObjectName = [NSString stringWithFormat:@"MobileDateTimePicker_%d", pickerId];
-
     UnitySendMessage([unityObjectName UTF8String], "PickerClosedEvent", [dateString UTF8String]);
 }
 
 UIDatePicker *datePicker;
 
-+ (void) DP_show:(int)mode secondNumber:(double)unix pickerId:(int)pickerId{
-    UIViewController *vc =  UnityGetGLViewController();
-    
-    CGRect toolbarTargetFrame = CGRectMake(0, vc.view.bounds.size.height-216-44, [self GetW], 44);
-    CGRect datePickerTargetFrame = CGRectMake(0, vc.view.bounds.size.height-216, [self GetW], 216);
-    CGRect darkViewTargetFrame = CGRectMake(0, vc.view.bounds.size.height-216-44, [self GetW], 260);
-    CGRect whiteViewTargetFrame = vc.view.bounds;
-    
-    UIView *darkView = [[UIView alloc] initWithFrame:CGRectMake(0, vc.view.bounds.size.height, [self GetW], 260)];
-    darkView.alpha = 1;
++ (void)DP_show:(int)mode secondNumber:(double)unix pickerId:(int)pickerId {
+    UIViewController *vc = UnityGetGLViewController();
 
+    CGRect toolbarTargetFrame = CGRectMake(0, vc.view.bounds.size.height - 260, [self GetW], 44);
+    CGRect datePickerTargetFrame = CGRectMake(0, vc.view.bounds.size.height - 216, [self GetW], 216);
+    CGRect darkViewTargetFrame = CGRectMake(0, vc.view.bounds.size.height - 260, [self GetW], 260);
+
+    // Create the darkView with a solid background
+    UIView *darkView = [[UIView alloc] initWithFrame:CGRectMake(0, vc.view.bounds.size.height, [self GetW], 260)];
+    darkView.alpha = 1; // Fully opaque
     if (@available(iOS 13.0, *)) {
         darkView.backgroundColor = [UIColor systemGray5Color];
     } else {
@@ -87,106 +65,81 @@ UIDatePicker *datePicker;
             darkView.backgroundColor = [UIColor whiteColor];
         }
     }
-
     darkView.tag = 9;
-    
-    UIView *whiteView = [[UIView alloc] initWithFrame:vc.view.bounds];
-    whiteView.alpha = 1;
-    whiteView.backgroundColor = [[UIColor alloc] initWithRed:1 green:1 blue:1 alpha:0];
-    whiteView.tag = 8;
-    
+
+    // Add a tap gesture to dismiss the date picker
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(DP_dismissDatePicker:)];
     [darkView addGestureRecognizer:tapGesture];
     [vc.view addSubview:darkView];
-    [whiteView addGestureRecognizer:tapGesture];
-    [vc.view addSubview:whiteView];
-    
-    
-    datePicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, vc.view.bounds.size.height+44, [self GetW], 216)];
-    // datePicker.tag = 10;
-    // Store the pickerId as a tag or use it in the message
-    datePicker.tag = pickerId; // Store the picker ID in the tag for later use.
-        
-    
+
+    datePicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, vc.view.bounds.size.height, [self GetW], 216)];
+    datePicker.tag = pickerId;
+
     [datePicker addTarget:self action:@selector(DP_changeDate:) forControlEvents:UIControlEventValueChanged];
+
     switch (mode) {
         case 1:
             datePicker.datePickerMode = UIDatePickerModeTime;
             break;
-            
         case 2:
             datePicker.datePickerMode = UIDatePickerModeDate;
-             if (@available(iOS 13.4, *)) {
+            if (@available(iOS 13.4, *)) {
                 datePicker.preferredDatePickerStyle = UIDatePickerStyleWheels;
             }
-            NSDate *dateTraded = [NSDate dateWithTimeIntervalSince1970 :unix];
-            [datePicker setDate:dateTraded];
+            NSDate *date = [NSDate dateWithTimeIntervalSince1970:unix];
+            [datePicker setDate:date];
             break;
-            
     }
-    
-    
-    // NSLog(@"dtp mode: %ld", (long)datePicker.datePickerMode);
-    
-    
     [vc.view addSubview:datePicker];
-    
-    UIToolbar *toolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, vc.view.bounds.size.height, [self GetW], 44)];
-    
-    toolBar.tag = 11;
-    toolBar.barStyle = UIBarStyleDefault;
+
+    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, vc.view.bounds.size.height, [self GetW], 44)];
+    toolbar.tag = 11;
+    toolbar.barStyle = UIBarStyleDefault;
     UIBarButtonItem *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
     UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(DP_dismissDatePicker:)];
 
-    
-    [toolBar setItems:[NSArray arrayWithObjects:spacer, doneButton, nil]];
-    [vc.view addSubview:toolBar];
-    
-    [UIView beginAnimations:@"MoveIn" context:nil];
-    toolBar.frame = toolbarTargetFrame;
-    datePicker.frame = datePickerTargetFrame;
-    darkView.frame = darkViewTargetFrame;
-    whiteView.frame = whiteViewTargetFrame;
-    
-    [UIView commitAnimations];
-    
+    [toolbar setItems:@[spacer, doneButton]];
+    [vc.view addSubview:toolbar];
+
+    [UIView animateWithDuration:0.3 animations:^{
+        toolbar.frame = toolbarTargetFrame;
+        datePicker.frame = datePickerTargetFrame;
+        darkView.frame = darkViewTargetFrame;
+    }];
 }
 
-+ (void)DP_removeViews:(id)object {
-    UIViewController *vc =  UnityGetGLViewController();
-    
-    [[vc.view viewWithTag:8] removeFromSuperview];
-    [[vc.view viewWithTag:9] removeFromSuperview];
-    [[vc.view viewWithTag:10] removeFromSuperview];
-    [[vc.view viewWithTag:11] removeFromSuperview];
+
+
++ (void)DP_removeViews {
+    UIViewController *vc = UnityGetGLViewController();
+    [[vc.view viewWithTag:9] removeFromSuperview];  // Dark view
+    [[vc.view viewWithTag:11] removeFromSuperview]; // Toolbar
+    [datePicker removeFromSuperview];              // Date picker
+    datePicker = nil;
 }
 
 + (void)DP_dismissDatePicker:(id)sender {
-    UIViewController *vc =  UnityGetGLViewController();
-    
-    [self DP_PickerClosed:datePicker];
-    
-    CGRect toolbarTargetFrame = CGRectMake(0, vc.view.bounds.size.height, [self GetW], 44);
-    CGRect datePickerTargetFrame = CGRectMake(0, vc.view.bounds.size.height+44, [self GetW], 216);
-    CGRect darkViewTargetFrame = CGRectMake(0, vc.view.bounds.size.height, [self GetW], 260);
-    
-    
-    [UIView beginAnimations:@"MoveOut" context:nil];
-    [vc.view viewWithTag:9].frame = darkViewTargetFrame;
-    [vc.view viewWithTag:10].frame = datePickerTargetFrame;
-    [vc.view viewWithTag:11].frame = toolbarTargetFrame;
-    [UIView setAnimationDelegate:self];
-    [UIView setAnimationDidStopSelector:@selector(DP_removeViews:)];
-    [UIView commitAnimations];
-}
+    UIViewController *vc = UnityGetGLViewController();
 
+    [self DP_PickerClosed:datePicker];
+
+    CGRect offscreenFrame = CGRectMake(0, vc.view.bounds.size.height, [self GetW], 260);
+
+    [UIView animateWithDuration:0.3 animations:^{
+        [vc.view viewWithTag:9].frame = offscreenFrame;
+        datePicker.frame = offscreenFrame;
+        [vc.view viewWithTag:11].frame = offscreenFrame;
+    } completion:^(BOOL finished) {
+        [self DP_removeViews];
+    }];
+}
 
 extern "C" {
     
     //--------------------------------------
     //  Unity Call Date Time Picker
     //--------------------------------------
-    
+
     void _TAG_ShowDatePicker(int mode, double unix, int pickerId) {
         [IOSNativeDatePicker DP_show:mode secondNumber:unix pickerId:pickerId];
     }
