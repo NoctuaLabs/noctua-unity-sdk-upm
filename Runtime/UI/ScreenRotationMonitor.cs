d231d3e8215c994a0a7c5572ac513d2d363fe2ee
Wijaya Adhi Surya <wijaya.adhisurya@gmail.com>
2024-12-24 18:41:57 +0700

    fix: scale should adjust with auto rotate

---
+++ Runtime/UI/ScreenRotationMonitor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace com.noctuagames.sdk.UI
+{
+    public class ScreenRotationMonitor : MonoBehaviour
+    {
+        private bool _isLandscape;
+        
+        public PanelSettings PanelSettings;
+        
+        private void Update()
+        {
+            if (_isLandscape == Screen.width > Screen.height) return;
+
+            _isLandscape = Screen.width > Screen.height;
+            
+            if (_isLandscape)
+            {
+                PanelSettings.match = 0;
+                PanelSettings.referenceResolution = new Vector2Int(800, 360);
+            }
+            else
+            {
+                PanelSettings.match = 1;
+                PanelSettings.referenceResolution = new Vector2Int(360, 800);
+            }
+        }
+    }
+}
