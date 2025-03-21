f905f91657a8b312d49c8c38fae94cfb5bb87aa0
EVOS-LP22-0007\User <mujiburrohman@noctua.gg>
2025-03-21 18:50:23 +0700

    feat: add RewardedAdmob class for managing AdMob Rewarded ads

---
+++ Runtime/AdsManager/Admob/RewardedAdmob.cs
@@ -0,0 +1,135 @@
+#if UNITY_ADMOB
+using GoogleMobileAds.Api;
+using UnityEngine;
+using System;
+
+namespace com.noctuagames.sdk.Admob
+{
+    public class RewardedAdmob
+    {
+        private readonly NoctuaLogger _log = new(typeof(InterstitialAdmob));
+        private string _adUnitIDRewarded;
+
+          private RewardedAd _rewardedAd;
+
+        /// <summary>
+        /// Loads the rewarded ad.
+        /// </summary>
+        public void LoadRewardedAd(string adUnitID)
+        {
+            if (adUnitID == null)
+            {
+                _log.Error("Ad unit ID Rewarded is empty.");
+                return;
+            }
+
+            _adUnitIDRewarded = adUnitID;
+            
+            // Clean up the old ad before loading a new one.
+            CleanupAd();
+
+            _log.Debug("Loading the rewarded ad.");
+
+            // create our request used to load the ad.
+            var adRequest = new AdRequest();
+
+            // send the request to load the ad.
+            RewardedAd.Load(_adUnitIDRewarded, adRequest,
+                (RewardedAd ad, LoadAdError error) =>
+                {
+                    // if error is not null, the load request failed.
+                    if (error != null || ad == null)
+                    {
+                        _log.Error("Rewarded ad failed to load an ad " +
+                                        "with error : " + error);
+                        return;
+                    }
+
+                    _log.Debug("Rewarded ad loaded with response : "
+                                + ad.GetResponseInfo());
+
+                    // // Create and pass the SSV options to the rewarded ad.
+                    // var options = new ServerSideVerificationOptions
+                    //                     .Builder()
+                    //                     .SetCustomData("SAMPLE_CUSTOM_DATA_STRING")
+                    //                     .Build();
+
+                    // ad.SetServerSideVerificationOptions(options);
+
+                    _rewardedAd = ad;
+                    RegisterEventHandlers(ad);
+                });
+        }
+
+        public void ShowRewardedAd()
+        {
+            const string rewardMsg =
+                "Rewarded ad rewarded the user. Type: {0}, amount: {1}.";
+
+            if (_rewardedAd != null && _rewardedAd.CanShowAd())
+            {
+                _rewardedAd.Show((Reward reward) =>
+                {
+                    // TODO: Reward the user.
+                    _log.Debug(String.Format(rewardMsg, reward.Type, reward.Amount));
+
+                    _log.Debug("Rewarded ad shown.");
+                });
+            }
+        }
+
+        private void RegisterEventHandlers(RewardedAd ad)
+        {
+            // Raised when the ad is estimated to have earned money.
+            ad.OnAdPaid += (AdValue adValue) =>
+            {
+                _log.Debug(String.Format("Rewarded ad paid {0} {1}.",
+                    adValue.Value,
+                    adValue.CurrencyCode));
+            };
+            // Raised when an impression is recorded for an ad.
+            ad.OnAdImpressionRecorded += () =>
+            {
+                _log.Debug("Rewarded ad recorded an impression.");
+            };
+            // Raised when a click is recorded for an ad.
+            ad.OnAdClicked += () =>
+            {
+                _log.Debug("Rewarded ad was clicked.");
+            };
+            // Raised when an ad opened full screen content.
+            ad.OnAdFullScreenContentOpened += () =>
+            {
+                _log.Debug("Rewarded ad full screen content opened.");
+            };
+            // Raised when the ad closed full screen content.
+            ad.OnAdFullScreenContentClosed += () =>
+            {
+                _log.Debug("Rewarded ad full screen content closed.");
+
+                LoadRewardedAd(_adUnitIDRewarded);
+            };
+            // Raised when the ad failed to open full screen content.
+            ad.OnAdFullScreenContentFailed += (AdError error) =>
+            {
+                _log.Error("Rewarded ad failed to open full screen content " +
+                            "with error : " + error);
+
+                LoadRewardedAd(_adUnitIDRewarded);
+            };
+        }
+
+        public void CleanupAd()
+        {
+            if (_rewardedAd != null)
+            {
+                _rewardedAd.Destroy();
+                _rewardedAd = null;
+
+                _log.Debug("Rewarded ad cleaned up.");
+            }
+        }
+    }
+}
+
+#endif
