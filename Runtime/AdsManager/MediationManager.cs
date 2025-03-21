a8d78e9876f8a3561618576924b1c568ca228295
EVOS-LP22-0007\User <mujiburrohman@noctua.gg>
2025-03-21 17:04:33 +0700

    feat: add MediationManager for unified mediation SDK handling

---
+++ Runtime/AdsManager/MediationManager.cs
@@ -0,0 +1,74 @@
+using System;
+
+#if UNITY_ADMOB
+using GoogleMobileAds.Api;
+#endif
+
+namespace com.noctuagames.sdk
+{
+    public class MediationManager
+    {
+        private readonly NoctuaLogger _log = new(typeof(MediationManager));
+        private IAdNetwork _adNetwork;
+
+        #if UNITY_ADMOB
+        public Action<AdValue> OnAdPaid;
+        #endif
+        public Action OnAdImpressionRecorded;
+        public Action OnAdClicked;
+        public Action OnAdFullScreenContentOpened;
+        public Action OnAdFullScreenContentClosed;
+
+        public void Initialize(Action initCompleteAction)
+        {
+            _log.Info("Initializing Ad Network");
+
+            #if !UNITY_ADMOB && !UNITY_APPLOVIN
+            _log.Error("Admob or AppLovin is not enabled. Please enable one of them.");
+            return;
+            #endif
+
+            #if UNITY_ADMOB
+            _adNetwork = new AdmobManager();
+            // _adNetwork.InterstitialOnAdPaid += OnAdPaid;
+            // _adNetwork.InterstitialOnAdImpressionRecorded += OnAdImpressionRecorded;
+            _adNetwork.InterstitialOnAdClicked += OnAdClicked;
+            _adNetwork.InterstitialOnAdFullScreenContentOpened += OnAdFullScreenContentOpened;
+            _adNetwork.InterstitialOnAdFullScreenContentClosed += OnAdFullScreenContentClosed;
+            #endif
+
+            // #if UNITY_APPLOVIN
+            // _adNetwork = new AppLovinManager();
+            // #endif
+
+            _adNetwork.Initialize(() => initCompleteAction?.Invoke());
+        }
+
+        public void LoadInterstitialAd(string adUnitID) => _adNetwork.LoadInterstitialAd(adUnitID);
+        public void ShowInterstitial() => _adNetwork.ShowInterstitial();
+        public void LoadRewardedAd(string adUnitID) => _adNetwork.LoadRewardedAd(adUnitID);
+        public void ShowRewardedAd() => _adNetwork.ShowRewardedAd();
+        public void SetBannerAdUnitId(string adUnitID) => _adNetwork.SetBannerAdUnitId(adUnitID);
+        #if UNITY_ADMOB
+        public void CreateBannerViewAdAdmob(AdSize adSize, AdPosition adPosition) => _adNetwork.CreateBannerViewAdAdmob(adSize, adPosition);
+        #endif
+        public void LoadBannerAd() => _adNetwork.LoadBannerAd();
+
+        public void OnIAADestroy()
+        {
+            if (_adNetwork != null)
+            {
+                #if UNITY_ADMOB
+                // if (OnAdPaid != null) _adNetwork.InterstitialOnAdPaid -= OnAdPaid;
+                #endif
+                // if (OnAdImpressionRecorded != null) _adNetwork.InterstitialOnAdImpressionRecorded -= OnAdImpressionRecorded;
+                // if (OnAdClicked != null) _adNetwork.InterstitialOnAdClicked -= OnAdClicked;
+                // if (OnAdFullScreenContentOpened != null) _adNetwork.InterstitialOnAdFullScreenContentOpened -= OnAdFullScreenContentOpened;
+                // if (OnAdFullScreenContentClosed != null) _adNetwork.InterstitialOnAdFullScreenContentClosed -= OnAdFullScreenContentClosed;
+
+                _adNetwork.OnDestroy();
+                // _adNetwork = null;
+            }
+        }
+    }
+}
