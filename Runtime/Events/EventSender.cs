a9da3ce45c89deb351bcd69d08dac4d661efea5e
EVOS-LP22-0007\User <mujiburrohman@noctua.gg>
2025-04-28 18:31:25 +0700

    fix event key not found

--- Runtime/Events/EventSender.cs
+++ Runtime/Events/EventSender.cs
@@ -259,7 +259,7 @@ namespace com.noctuagames.sdk.Events
             }
             _log.Info($"{name} added to the queue. Current total event in queue: {events.Count}");
 
-            if (data.event_name != "offline")
+            if (data.TryGetValue("event_name", out var eventName) && eventName.ToString() != "offline")
             {
                 Noctua.IsOfflineAsync().ContinueWith((isOffline) =>
                 {
