895363b896dc3de99a16425a4bfc9c9524d6f35b
Herpiko Dwi Aguno <herpiko@noctuagames.com>
2025-05-05 15:44:24 +0000

    Edit EventSender.cs, decrease event sender internal from 5 minutes to 1 minute.

--- Runtime/Events/EventSender.cs
+++ Runtime/Events/EventSender.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -19,7 +19,7 @@ namespace com.noctuagames.sdk.Events
         public string BundleId = Application.identifier;
         public uint BatchSize = 20;
         public uint MaxBatchSize = 100;
-        public uint BatchPeriodMs = 300_000; // 5 minute
+        public uint BatchPeriodMs = 60_000; // 1 minute
     }
     
     [Preserve]
