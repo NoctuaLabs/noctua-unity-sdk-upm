04f53cbfa4ab0657640690d3827eb8897c67e48b
EVOS-LP22-0007\User <mujiburrohman@noctua.gg>
2025-06-10 15:59:16 +0700

    fix: issue Collection was modified, enumeration operation may not execute on event sender

--- Runtime/Events/EventSender.cs
+++ Runtime/Events/EventSender.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -37,7 +38,7 @@ namespace com.noctuagames.sdk.Events
         private readonly ILogger _log = new NoctuaLogger(typeof(EventSender));
         private readonly EventSenderConfig _config;
         private readonly NoctuaLocale _locale;
-        private List<Dictionary<string, IConvertible>> _eventQueue;
+        private ConcurrentQueue<Dictionary<string, IConvertible>> _eventQueue;
         private readonly UniTask _sendTask;
         private readonly CancellationTokenSource _cancelSendSource;
         private readonly DateTime _start;
@@ -142,81 +143,68 @@ namespace com.noctuagames.sdk.Events
         private void LoadEventsFromPlayerPrefs()
         {
             _log.Info("Loading NoctuaEvents from PlayerPrefs to event queue");
-            var eventsJson = PlayerPrefs.GetString("NoctuaEvents", "[]");
-            if (eventsJson == null)
-            {
-                eventsJson = "[]";
-            }
+
+            string eventsJson = PlayerPrefs.GetString("NoctuaEvents", "[]");
 
             if (eventsJson.Length > 800000) // Around 1000 events
             {
-                _log.Info("NoctuaEvents is too large, clearing it");
+                _log.Warning("NoctuaEvents too large, clearing PlayerPrefs to prevent issues");
                 PlayerPrefs.SetString("NoctuaEvents", "[]");
                 PlayerPrefs.Save();
                 eventsJson = "[]";
             }
-            
 
             // Try to parse into IConvertible first because it is
             // the native type of the queue.
             // There will be nested try catch to make it safe.
             var events = new List<Dictionary<string, IConvertible>>();
-            //_log.Debug(eventsJson);
+
             try
             {
+                // First try parsing directly into convertible dictionaries
                 events = JsonConvert.DeserializeObject<List<Dictionary<string, IConvertible>>>(eventsJson);
             }
             catch (Exception e)
             {
-                events = new List<Dictionary<string, IConvertible>>();
-                _log.Error($"Failed to load events from PlayerPrefs: {e.Message}.");
-
                 // If fail, try to parse to object.
                 // IConvertible cannot parse null value from JSON.
                 // Load from PlayerPrefs and re-enqueue them all
-                _log.Info("Try to parse NoctuaEvents with object type");
-                var objects = new List<Dictionary<string, object>>();
+                
+                _log.Warning($"Failed to parse events as IConvertible: {e.Message}");
+
+                // Fallback to generic object parsing
+                _log.Info("Trying fallback JSON parse as List<Dictionary<string, object>>");
                 try
                 {
-                    objects = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(eventsJson);
+                    var objectEvents = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(eventsJson);
+
+                    foreach (var evt in objectEvents)
+                    {
+                        var convertedEvent = new Dictionary<string, IConvertible>();
+                        foreach (var (key, val) in evt)
+                        {
+                            if (val is IConvertible convertible)
+                            {
+                                convertedEvent[key] = convertible;
+                            }
+                            else
+                            {
+                                _log.Warning($"Skipping non-convertible key: {key}, value: {val}");
+                            }
+                        }
+                        events.Add(convertedEvent);
+                    }
                 }
                 catch (Exception e2)
                 {
-                    _log.Error($"Failed to load events from PlayerPrefs: {e2.Message}");
-                    objects = new List<Dictionary<string, object>>();
-                }
-                if (objects == null)
-                {
-                    objects = new List<Dictionary<string, object>>();
-                }
-                foreach (var evt in objects)
-                {
-                    var dict = new Dictionary<string, IConvertible>();
-                    foreach (var (key, val) in evt)
-                    {
-                        if (val is IConvertible convertible)
-                        {
-                            dict[key] = convertible;
-                        }
-                        else
-                        {
-                            _log.Warning($"Event has non-convertible value for key {key} of value {val}");
-                        }
-                    }
-
-                    // Per object iteration, add the dict to events list
-                    events.Add(dict);
+                    _log.Error($"Fallback JSON parse failed: {e2.Message}");
                 }
             }
-            if (events == null)
-            {
-                events = JsonConvert.DeserializeObject<List<Dictionary<string, IConvertible>>>(eventsJson);
-            }
-            _log.Info($"Total loaded events from PlayerPrefs: {events.Count}");
 
-            _eventQueue = new List<Dictionary<string, IConvertible>>(events);
+            _log.Info($"Total events loaded into queue: {events.Count}");
 
-            _log.Info($"Total loaded events from PlayerPrefs: {_eventQueue.Count}");
+            _eventQueue = new ConcurrentQueue<Dictionary<string, IConvertible>>(events);
+            _log.Info($"Concurrent queue initialized with {_eventQueue.Count} events");
         }
 
         public void Send(string name, Dictionary<string, IConvertible> data = null)
@@ -244,58 +232,68 @@ namespace com.noctuagames.sdk.Events
             // Fire-and-forget safe background task
             UniTask.Void(async () =>
             {
-                await UniTask.SwitchToMainThread();
-
-                data.TryAdd("event_version", 1);
-                data.TryAdd("event_name", name);
-                data.TryAdd("sdk_version", _sdkVersion);
-                data.TryAdd("device_id", _deviceId);
-                data.TryAdd("device_os_version", SystemInfo.operatingSystem);
-                data.TryAdd("device_os", SystemInfo.operatingSystemFamily.ToString());
-                data.TryAdd("device_type", SystemInfo.deviceType.ToString());
-                data.TryAdd("device_model", SystemInfo.deviceModel);
-                data.TryAdd("bundle_id", _config.BundleId);
-                data.TryAdd("game_version", Application.version);
-                data.TryAdd("country", _locale.GetCountry());
-                data.TryAdd("ipAddress", _ipAddress);
-                data.TryAdd("is_sandbox", _isSandbox);
-
-                LastEventTime = _start.AddSeconds(Stopwatch.GetTimestamp() / (double)Stopwatch.Frequency);
-                data.TryAdd("timestamp", LastEventTime.ToString("O"));
-
-                if (_userId != null) data.TryAdd("user_id", _userId);
-                if (_playerId != null) data.TryAdd("player_id", _playerId);
-                if (_credentialId != null) data.TryAdd("credential_id", _credentialId);
-                if (_credentialProvider != null) data.TryAdd("credential_provider", _credentialProvider);
-                if (_gameId != null) data.TryAdd("game_id", _gameId);
-                if (_gamePlatformId != null) data.TryAdd("game_platform_id", _gamePlatformId);
-                if (_sessionId != null) data.TryAdd("session_id", _sessionId);
-                if (_uniqueId != null) data.TryAdd("unique_id", _uniqueId);
-
-                _log.Info($"queued event '{LastEventTime:O}|{name}|{_deviceId}|{_sessionId}|{_userId}|{_playerId}'");
-
-                _eventQueue.Add(data);
-
-                PlayerPrefs.SetString("NoctuaEvents", JsonConvert.SerializeObject(_eventQueue));
-                PlayerPrefs.Save();
-                _log.Info($"{name} added to the queue. Current total event in queue: {_eventQueue.Count}");
-
-                // This check is used to maintain the offline state more frequent to update.
-                // This also prevent "offline" event flooding the queue
-                // by not sending another "offline" event if the event name is "offline"
-                if (data.TryGetValue("event_name", out var eventName) && eventName.ToString() != "offline")
+                try
                 {
-                    Noctua.IsOfflineAsync().ContinueWith((isOffline) =>
+                    await UniTask.SwitchToMainThread();
+
+                    data.TryAdd("event_version", 1);
+                    data.TryAdd("event_name", name);
+                    data.TryAdd("sdk_version", _sdkVersion);
+                    data.TryAdd("device_id", _deviceId);
+                    data.TryAdd("device_os_version", SystemInfo.operatingSystem);
+                    data.TryAdd("device_os", SystemInfo.operatingSystemFamily.ToString());
+                    data.TryAdd("device_type", SystemInfo.deviceType.ToString());
+                    data.TryAdd("device_model", SystemInfo.deviceModel);
+                    data.TryAdd("bundle_id", _config.BundleId);
+                    data.TryAdd("game_version", Application.version);
+                    data.TryAdd("country", _locale.GetCountry());
+                    data.TryAdd("ipAddress", _ipAddress);
+                    data.TryAdd("is_sandbox", _isSandbox);
+
+                    LastEventTime = _start.AddSeconds(Stopwatch.GetTimestamp() / (double)Stopwatch.Frequency);
+                    data.TryAdd("timestamp", LastEventTime.ToString("O"));
+
+                    if (_userId != null) data.TryAdd("user_id", _userId);
+                    if (_playerId != null) data.TryAdd("player_id", _playerId);
+                    if (_credentialId != null) data.TryAdd("credential_id", _credentialId);
+                    if (_credentialProvider != null) data.TryAdd("credential_provider", _credentialProvider);
+                    if (_gameId != null) data.TryAdd("game_id", _gameId);
+                    if (_gamePlatformId != null) data.TryAdd("game_platform_id", _gamePlatformId);
+                    if (_sessionId != null) data.TryAdd("session_id", _sessionId);
+                    if (_uniqueId != null) data.TryAdd("unique_id", _uniqueId);
+
+                    _log.Info($"queued event '{LastEventTime:O}|{name}|{_deviceId}|{_sessionId}|{_userId}|{_playerId}'");
+
+                    // Deprecated: Use ConcurrentQueue to avoid blocking the main thread
+                    // _eventQueue.Add(data);
+
+                    _eventQueue.Enqueue(data);
+
+                    PlayerPrefs.SetString("NoctuaEvents", JsonConvert.SerializeObject(_eventQueue));
+                    PlayerPrefs.Save();
+                    _log.Info($"{name} added to the queue. Current total event in queue: {_eventQueue.Count}");
+
+                    // This check is used to maintain the offline state more frequent to update.
+                    // This also prevent "offline" event flooding the queue
+                    // by not sending another "offline" event if the event name is "offline"
+                    if (data.TryGetValue("event_name", out var eventName) && eventName.ToString() != "offline")
                     {
-                        if (isOffline)
+                        _ = Noctua.IsOfflineAsync().ContinueWith((isOffline) =>
                         {
-                            Noctua.OnOffline();
-                        }
-                        else
-                        {
-                            Noctua.OnOnline();
-                        }
-                    });
+                            if (isOffline)
+                            {
+                                Noctua.OnOffline();
+                            }
+                            else
+                            {
+                                Noctua.OnOnline();
+                            }
+                        });
+                    }
+                }
+                catch (Exception e)
+                {
+                    _log.Warning($"Failed to enqueue or persist event {name}: {e}");
                 }
             });
         }
@@ -341,39 +339,50 @@ namespace com.noctuagames.sdk.Events
 
         public void Flush()
         {
-#if UNITY_IOS && !UNITY_EDITOR
+        #if UNITY_IOS && !UNITY_EDITOR
             // This patch only applied for IOS to cover Sortify specific crash
-            // where the HTTP request cause crash when the app is trying to quit
-            _log.Debug("On Flush called on IOS. Abort to avoid crash");
+            // where the HTTP request causes crash when the app is trying to quit
+            _log.Debug("On Flush called on iOS. Abort to avoid crash");
             return;
 
             // No need to backup to PlayerPrefs. The latest backup from Send() is already sufficient
-#endif
+        #endif
 
-            _log.Debug("On Flush called. " + $"Current total event in queue: {_eventQueue.Count}");
+            _log.Debug($"On Flush called. Current total events in queue: {_eventQueue.Count}");
+
+            // Snapshot the queue contents in a thread-safe way
+            var snapshot = _eventQueue.ToArray();
 
             var request = new HttpRequest(HttpMethod.Post, $"{_config.BaseUrl}/events")
                 .WithHeader("X-CLIENT-ID", _config.ClientId)
                 .WithHeader("X-DEVICE-ID", SanitizeHeaderValue(_deviceId))
-                .WithNdjsonBody(_eventQueue);
+                .WithNdjsonBody(snapshot);
 
             UniTask.Void(async () =>
             {
                 try
                 {
                     await request.Send<EventResponse>();
+
                     // All dequeued events is sent successfuly to server,
                     // then it's safe to remove all items from PlayerPrefs
+                    // Clear the queue only after success
+                    while (_eventQueue.TryDequeue(out _)) { }
+
+                    await UniTask.SwitchToMainThread();
+
+                    // Clear PlayerPrefs after successful send
                     PlayerPrefs.SetString("NoctuaEvents", "[]");
                     PlayerPrefs.Save();
-                    _eventQueue.Clear();
-                    _log.Info($"Sent {_eventQueue.Count} events. PlayerPrefs cleared.");
+
+                    _log.Info($"Sent {snapshot.Length} events. PlayerPrefs cleared.");
                 }
                 catch (Exception e)
                 {
                     // No need to backup to PlayerPrefs. The latest backup from Send() is already sufficient.
                     // No need to re-enqueue because the clearing queue part is on the success branch above
                     // Simply print the error as warning
+                    // We don't clear the queue on failure
                     _log.Warning("Failed to send events: " + e.Message);
                 }
             });
@@ -403,21 +412,20 @@ namespace com.noctuagames.sdk.Events
                 await UniTask.Delay(_config.CycleDelay, cancellationToken: token);
 
                 // If the queue is empty, wait for another 1 sec
-                while (_eventQueue.Count == 0)
+                while (_eventQueue.IsEmpty)
                 {
-                    await UniTask.Delay(1000, cancellationToken: token);
-                }
-                var nextBatchSchedule = DateTime.UtcNow.AddMilliseconds(_config.BatchPeriodMs);
-                // If the queue length is less than batch size
-                while (_eventQueue.Count < _config.BatchSize &&
-                // or it is not reached the next batch schedule yet
-                DateTime.UtcNow < nextBatchSchedule)
-                {
-                    // Then wait for another 1 sec.
                     await UniTask.Delay(1000, cancellationToken: token);
                 }
 
-                if (_eventQueue.Count == 0)
+                var nextBatchSchedule = DateTime.UtcNow.AddMilliseconds(_config.BatchPeriodMs);
+
+                // If the queue length is less than batch size or it is not reached the next batch schedule yet
+                while (_eventQueue.Count < _config.BatchSize && DateTime.UtcNow < nextBatchSchedule)
+                {
+                    await UniTask.Delay(1000, cancellationToken: token);
+                }
+
+                if (_eventQueue.IsEmpty)
                 {
                     // If the queue is still empty, immediately return.
                     // At this point, the maximum delay for a cycle is "CycleDelay" + 2 seconds = 7 seconds
@@ -429,25 +437,20 @@ namespace com.noctuagames.sdk.Events
                 if (isOffline)
                 {
                     Noctua.OnOffline();
-
-                    _log.Info($"Device is offline, continue to next cycle");
+                    _log.Info("Device is offline, continue to next cycle");
                     continue;
                 }
-                
+
                 Noctua.OnOnline();
 
-                // Dequeue to be sent to server
+                // Build batch from the queue
                 var events = new List<Dictionary<string, IConvertible>>();
-                if (_eventQueue.Count <= _config.MaxBatchSize)
+                while (events.Count < _config.MaxBatchSize && _eventQueue.TryDequeue(out var item))
                 {
-                    events = new List<Dictionary<string, IConvertible>>(_eventQueue);
-                    _eventQueue.Clear();
-                } else {
-                    events = _eventQueue.GetRange(0, _config.MaxBatchSize);
-                    _eventQueue.RemoveRange(0, _config.MaxBatchSize);
+                    events.Add(item);
                 }
 
-                _log.Info($"Batch size: {_config.MaxBatchSize}, events to be send: {events.Count}, events in the queue: {_eventQueue.Count}");
+                _log.Info($"Batch size: {_config.MaxBatchSize}, events to be sent: {events.Count}, events remaining in queue: {_eventQueue.Count}");
 
                 try
                 {
@@ -457,16 +460,21 @@ namespace com.noctuagames.sdk.Events
                         .WithNdjsonBody(events);
 
                     await request.Send<EventResponse>();
-                    _log.Info($"Sent {events.Count} events. Events in queue: {_eventQueue.Count}");
+                    _log.Info($"Sent {events.Count} events. Events left in queue: {_eventQueue.Count}");
                 }
                 catch (Exception e)
                 {
                     _log.Error($"Failed to send events to server: {e.Message}");
-                    // Re-enqueue all the events
-                    _eventQueue.AddRange(events);
+
+                    // Re-enqueue failed events
+                    foreach (var ev in events)
+                    {
+                        _eventQueue.Enqueue(ev);
+                    }
                 }
             }
         }
+
         
         private string SanitizeHeaderValue(string value)
         {
