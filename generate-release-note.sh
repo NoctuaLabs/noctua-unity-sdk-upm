9df4816a00d4d01b96870b0ab75fa8b7c84c81e0
Herpiko Dwi Aguno <herpiko@gmail.com>
2026-01-29 17:49:55 +0700

    Remove script files that does not have meta file.

--- generate-release-note.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git-cliff --latest > GithubRelease.md
-[ "$(wc -l < GithubRelease.md)" -lt 2 ] && git-cliff | awk '/^## \[/{if (NR!=1) exit} {print}' > GithubRelease.md
-cat GithubRelease.md
--- icm-version-suffix.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-grep -q 'icm' Runtime/AssemblyInfo.cs || sed -i.bak 's/\(AssemblyVersion("\)\([^"]*\)\(")\)/\1\2-icm\3/' Runtime/AssemblyInfo.cs; grep -q 'icm' package.json || sed -i.bak 's/\("version": "\)\([^"]*\)\(".*\)/\1\2-icm\3/' package.json
